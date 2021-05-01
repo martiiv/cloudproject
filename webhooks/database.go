0d6484eb77a2baeccd45aef7cb05b1ccee578843
Martin Iversen <martiiv@ntnu.no>
2021-05-01 11:37:51 +0200

    Updated project structure with webhook directory containing all webhook and database functionality

---
+++ webhooks/database.go
@@ -0,0 +1,12 @@
+package webhooks
+
+/**
+ * Class database.go
+ * Will contain all database related functionality
+ * Contains the following functions:
+ *
+ *
+ * @author Martin Iversen
+ * @date 01.05.2021
+ * @version 0.1
+ */
---
+++ webhooks/webhooks.go
@@ -0,0 +1,12 @@
+package webhooks
+
+/**
+ * Class webhooks.go
+ * Will contain all webhooks functionality for the application (may get separated into more files)
+ * Will contain the following funcitons:
+ *										Handler
+ *
+ * @author Martin Iversen
+ * @date 01.05.2021
+ * @version 0.1
+ */

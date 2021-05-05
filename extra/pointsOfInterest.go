cff18b4fb5b8f1de230227dfd5d74d09968bcbd5
karin_pettersen <karinep@stud.ntnu.no>
2021-05-05 14:04:16 +0200

    Fixed error messages

--- extra/pointsOfInterest.go
+++ extra/pointsOfInterest.go
@@ -12,20 +12,20 @@ import (
 //Function that will display all the points of interest from a location, within 1km
 func PointOfInterest(w http.ResponseWriter, request *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	address := strings.Split(request.URL.Path, `/`)[2] //Getting the address/name of the place we want to look for points of interest
 
-	poi123 := strings.Split(request.URL.Path, `/`)[3]
+	address := strings.Split(request.URL.Path, `/`)[2] //Getting the address/name of the place we want to look for points of interest
+	poiPath := strings.Split(request.URL.Path, `/`)[3]
 
 	latitude, longitude, err := GetLocation(url.QueryEscape(address)) //Receives the latitude and longitude of the place passed in the url
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "ERROR, The searched place does not exist", http.StatusBadRequest)
 		return
 	}
 
-	response, err := http.Get("https://api.tomtom.com/search/2/poiSearch/" + poi123 + ".json?lat=" + latitude + "&lon=" + longitude + "&radius=1000&key=gcP26xVobGHjX2VVWGTskjelxX81WA1G")
+	response, err := http.Get("https://api.tomtom.com/search/2/poiSearch/" + poiPath + ".json?lat=" + latitude + "&lon=" + longitude + "&radius=1000&key=gcP26xVobGHjX2VVWGTskjelxX81WA1G")
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "ERROR, The searched point of interest does not exist", http.StatusBadRequest)
 		return
 
 	}
@@ -33,7 +33,7 @@ func PointOfInterest(w http.ResponseWriter, request *http.Request) {
 	var poi pointsOfInterest
 
 	if err = json.Unmarshal(body, &poi); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "ERROR, Internal server error", http.StatusInternalServerError)
 		return
 	}
 
@@ -50,7 +50,7 @@ func PointOfInterest(w http.ResponseWriter, request *http.Request) {
 
 	output, err := json.Marshal(total) //Marshalling the array to JSON
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "ERROR, Internal server error", http.StatusInternalServerError)
 		return
 	}
 

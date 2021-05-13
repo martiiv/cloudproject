6d490e6a54142b64fe4a55c4ef7d25fbb5aad7e3
Sander Island <sanderi@stud.ntnu.no>
2021-05-13 14:15:21 +0200

    Changed return to boolean

--- utils/validation.go
+++ utils/validation.go
@@ -6,16 +6,16 @@ import (
 )
 
 // Checks of the arrival time input is valid for the webhook registration
-func isValidInput(arrivalTime string) string {
+func isValidInput(arrivalTime string) bool {
 	if len(arrivalTime) != 0 {
 		aTime, err := time.Parse(time.RFC822, arrivalTime)
 		if err != nil {
 			fmt.Println(aTime)
-			return err.Error()
+			return false
 		}
-		return arrivalTime
+		return true
 	} else {
 		fmt.Println("Arrival time cannot be null")
+		return false
 	}
-	return arrivalTime
 }

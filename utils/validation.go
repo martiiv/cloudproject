9deaa2e217ac94554e5bd93ed69dc61b9d174e2e
tormodmm <tormodmm@stud.ntnu.no>
2021-05-14 10:02:20 +0200

    Commented and updated isValidInput in validation.go.

--- utils/validation.go
+++ utils/validation.go
@@ -1,21 +1,22 @@
 package utils
 
 import (
-	"fmt"
+	"log"
 	"time"
 )
 
-// Checks of the arrival time input is valid for the webhook registration
+// isValidInput Checks if the arrival time input is valid for the webhook registration.
+// Needs to follow the RFC822 format.
 func isValidInput(arrivalTime string) bool {
 	if len(arrivalTime) != 0 {
-		aTime, err := time.Parse(time.RFC822, arrivalTime)
+		// Tries to parse the arrivalTime to the RFC822-format
+		_, err := time.Parse(time.RFC822, arrivalTime)
 		if err != nil {
-			fmt.Println(aTime)
 			return false
 		}
 		return true
 	} else {
-		fmt.Println("Arrival time cannot be null")
+		log.Println("There was an error parsing the arrival time: " + arrivalTime + " to the RFC822-format.")
 		return false
 	}
 }

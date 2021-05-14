601652d0c84360a69bf6ecef43d11cc02aac9eb7
tormodmm <tormodmm@stud.ntnu.no>
2021-05-14 09:53:27 +0200

    Commented the API keys

--- utils/keys.go
+++ utils/keys.go
@@ -1,6 +1,13 @@
 package utils
 
+// TomtomKey The API key needed to access the TomTom API
 const TomtomKey = "gOorFpmbH5GPKh6uGqcfJN76oKFKfswA"
+
+// OpenRouteServiceKey The API key needed to access the Open Route Service API
 const OpenRouteServiceKey = "5b3ce3597851110001cf6248917ff3438230403186605e2ad94a71b6"
+
+// MapQuestKey The API key needed to access the Map Quest API
 const MapQuestKey = "UvCctIMBPNYcpfiAkTCkVjakeCjoPpPR"
+
+// OpenweathermapKey The API key needed to access the Openweathermap API
 const OpenweathermapKey = "92721f2c7ecab4f083189daef6b7f146"
